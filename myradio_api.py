7bf3fea96946546a1e4f374a76247be4ea90e982
Colin Roitt <roittcolin@gmail.com>
2025-04-23 19:43:41 +0100

    output response text for logging

--- myradio_api.py
+++ myradio_api.py
@@ -34,6 +34,7 @@ def get_all_terms(current_only=False):
     if current_only:
         params = "currentOnly=true"
     response = requests.get(f"{BASE_URL}/term/allterms?" + params + KEY_STRING, verify=should_verify)
+    print(response.text)
     return response.json()
 
 def get_current_term_id():
@@ -50,6 +51,7 @@ def get_show_application(season_id):
 
 def get_season_applied_weeks(season_id):
     response = requests.get(f"{BASE_URL}/season/{season_id}/requestedweeks?" + KEY_STRING, verify=should_verify)
+    print(response.text)
     weeks_applied_index = response.json()["payload"]
     term = get_all_terms(current_only=False)["payload"][-1]
     week_names = term["week_names"]
@@ -65,24 +67,29 @@ def get_season_applied_weeks(season_id):
 
 def get_season_applied_times(season_id):
     response = requests.get(f"{BASE_URL}/season/{season_id}/requestedtimesavail?" + KEY_STRING, verify=should_verify)
+    print(response.text)
     times = response.json()["payload"]
     return times
 
 def count_pending_allocations():
     response = requests.get(f"{BASE_URL}/scheduler/countpendingallocations?" + KEY_STRING, verify=should_verify)
+    print(response.text)
     return response.json()
 
 def pending_allocations():
     req_url = f"{BASE_URL}/scheduler/pendingallocations?" + KEY_STRING
     response = requests.get(req_url, verify=should_verify)
+    print(response.text)
     return response.json()
 
 def get_schedule(year, week):
     response = requests.get(f"{BASE_URL}/timeslot/weekschedule/{week}?year={year}" + KEY_STRING, verify=should_verify)
+    print(response.text)
     return response.json()
 
 def get_timeslot_info(timeslot_id):
     response = requests.get(f"{BASE_URL}/timeslot/{timeslot_id}?" + KEY_STRING, verify=should_verify)
+    print(response.text)
     return response.json()
 
 def check_for_conflicts(day, start, duration, term_id = None):
@@ -99,8 +106,7 @@ def check_for_conflicts(day, start, duration, term_id = None):
         "duration": int(duration)
     })
     response = requests.get(f"{BASE_URL}/scheduler/scheduleconflicts?term_id={term_id}&time[day]={day}&time[start]={start}&time[duration]={duration}" + KEY_STRING, verify=should_verify)
-    print(f"{BASE_URL}/scheduler/scheduleconflicts?term_id={term_id}&time[day]={day}&time[start]={start}&time[duration]={duration}" + KEY_STRING)
-    print("HAVE CHECKED: ", response.text)
+    print(response.text)
     return response.json()
 
 # -------- put --------
@@ -117,9 +123,8 @@ def schedule_season(season_id, data, headers):
             'timecustom_etime': data['timecustom_etime']
         }
     })
-    print(data_to_send, season_id)
     response = requests.put(f"{BASE_URL}/season/{season_id}/schedule?" + KEY_STRING, data=data_to_send, headers=headers, verify=should_verify)
-    print("HAVE ALLOCATED: ", response.text)
+    print(response.text)
     return response.json()
 
 def reject_season(season_id, reason, notify_user, headers):
@@ -129,7 +134,7 @@ def reject_season(season_id, reason, notify_user, headers):
     }
     data_to_send = json.dumps(data)
     response = requests.put(f"{BASE_URL}/season/{season_id}/reject?" + KEY_STRING, data=data_to_send, headers=headers, verify=should_verify)
-    print("HAVE REJECTED: ", response.text)
+    print(response.text)
     return response.json()
 
 def cancel_timeslot(timeslot_id, reason, headers):
@@ -138,6 +143,7 @@ def cancel_timeslot(timeslot_id, reason, headers):
     }
     data_to_send = json.dumps(data) 
     response = requests.put(f"{BASE_URL}/timeslot/{timeslot_id}/canceltimeslot?" + KEY_STRING, data=data_to_send, headers=headers, verify=should_verify)
+    print(response.text)
     return response.json()
 
 def move_timeslot(timeslot_id, new_start, new_end, headers):
@@ -147,4 +153,5 @@ def move_timeslot(timeslot_id, new_start, new_end, headers):
     }
     data_to_send = json.dumps(data)
     response = requests.put(f"{BASE_URL}/timeslot/{timeslot_id}/movetimeslot?" + KEY_STRING, data=data_to_send, headers=headers, verify=should_verify)
+    print(response.text)
     return response.json()
